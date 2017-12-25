640a2f25ce3775bb0ad86e79341c15cb7635d5f7
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-12-25 11:03:48 +0700

    Update doc comment

--- index.ts
+++ index.ts
@@ -3,26 +3,25 @@
  * https://basarat.gitbooks.io/typescript/docs/types/discriminated-unions.html
  *
  * @example
- * <pre><code>
- * type A = {type: 'a'};
- * type B = {type: 'b'};
- * type Union = A | B;
  *
- * function doSomething(arg: Union) {
- *   if (arg.type === 'a') {
- *     return something;
- *   }
+ *    type A = {type: 'a'};
+ *    type B = {type: 'b'};
+ *    type Union = A | B;
  *
- *   if (arg.type === 'b') {
- *     return somethingElse;
- *   }
+ *    function doSomething(arg: Union) {
+ *      if (arg.type === 'a') {
+ *        return something;
+ *      }
  *
- *   // TS will error if there are other types in the union
- *   // Will throw an Error when called at runtime. Use `assertNever(arg, true)`
- *   // instead to fail silently.
- *   return assertNever(arg);
- * }
- * </code></pre>
+ *      if (arg.type === 'b') {
+ *        return somethingElse;
+ *      }
+ *
+ *      // TS will error if there are other types in the union
+ *      // Will throw an Error when called at runtime.
+ *      // Use `assertNever(arg, true)` instead to fail silently.
+ *      return assertNever(arg);
+ *    }
  */
 export default function assertNever(value: never, noThrow?: boolean): never {
   if (noThrow) {
